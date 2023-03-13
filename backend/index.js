bec45e595fb8b666c23888545844d801cd8d8eaf
kesaraneet <42940788+kesaraneet@users.noreply.github.com>
2023-03-13 18:15:57 +0700

    chore: deploy prepare

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,25 @@ import productRouters from "./routes/productRouters.js";
 import userRouters from "./routes/userRouters.js";
 import authRouters from "./routes/authRouters.js";
 import config from "./config.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use("/product", productRouters.routes);
 app.use("/user", userRouters.routes);
 app.use("/auth", authRouters.routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
 
 app.listen(config.port, () => {
   console.log("Start server at port 8000.");
